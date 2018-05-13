bdbfb08f80d8d82ca4c3835b40edcd41978f7d88
ZackAkil <zackakil94@gmail.com>
2018-05-13 23:10:10 +0100

    added static hypothesis strcture

--- arduino code/lamp_learner/lamp_learner.ino
+++ arduino code/lamp_learner/lamp_learner.ino
@@ -16,6 +16,12 @@ int sensorValue = 0;
 
 bool ledState = false;
 
+float learnt_coef = 0.001;
+float learnt_intercept = 0.001;
+
+int sensorValStore[10];
+int stateStore[10];
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(button, INPUT);
@@ -28,26 +34,34 @@ bool buttonPressed(){
   return digitalRead(button);
 }
 
-void saveDataPoint(){
-  Serial.println("hello");
+void saveDataPoint(int sensorVal, bool state){
+
+  Serial.println(sensorVal);
+  Serial.println(state);
+}
+
+bool predict(int sensorVal){
+
+  bool state_prediction =  (sensorVal * learnt_coef + learnt_intercept) > 0.8 ;
+
+  return state_prediction;
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
-  sensorValue = analogRead(sensor);
+  
+ sensorValue = analogRead(sensor);
 
   if (buttonPressed()){
     while(buttonPressed()){
       delay(10);
     }
-    saveDataPoint();
 
     ledState = !ledState;
 
-    Serial.println(ledState);
-
-    digitalWrite(led, ledState);
+    saveDataPoint(sensorValue, ledState);
   }
-  
+
+  digitalWrite(led, predict(sensorValue));
 }
