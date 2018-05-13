a00289f39131cb1a30dc62a924e705ce27942510
ZackAkil <zackakil94@gmail.com>
2018-05-13 22:48:24 +0100

    added led toggle

--- arduino code/lamp_learner/lamp_learner.ino
+++ arduino code/lamp_learner/lamp_learner.ino
@@ -14,14 +14,40 @@ const int led = 9;
 
 int sensorValue = 0;
 
+bool ledState = false;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(button, INPUT);
   pinMode(led, OUTPUT);
+
+  Serial.begin(9600);
 }
+
+bool buttonPressed(){
+  return digitalRead(button);
+}
+
+void saveDataPoint(){
+  Serial.println("hello");
+}
+
 
 void loop() {
   // put your main code here, to run repeatedly:
   sensorValue = analogRead(sensor);
-  digitalWrite(led, HIGH);
+
+  if (buttonPressed()){
+    while(buttonPressed()){
+      delay(10);
+    }
+    saveDataPoint();
+
+    ledState = !ledState;
+
+    Serial.println(ledState);
+
+    digitalWrite(led, ledState);
+  }
+  
 }
